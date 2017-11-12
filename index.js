55e3001391423cba2c261434305ff4add2105e24
Steven Xu <steven@nexxt.in>
2017-11-12 00:21:32 -0500

    solve challenge

---
+++ index.js
@@ -0,0 +1,17 @@
+const getPixels = require("get-pixels")
+
+getPixels("http://www.northern.co/wp-content/themes/northern/images/puzzle.png", function(err, pixels) {
+  if(err) {
+    console.log("Bad image path")
+    return
+  }
+
+  let str = "";
+  pixels.data.forEach((pixel, i) => {
+    if (i % 4 !== 3) {
+      // discard alpha channel
+      str += String.fromCharCode(pixel)
+    }
+  })
+  console.log(str);
+})
